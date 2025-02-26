512ce7bbfc729218497e0ee795cda8ef6dfd4abc
Amirthawarshini <varshhhh08@gmail.com>
2025-02-26 15:30:04 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+my_list = [1,3,5,2,4]
+
+def operations():
+    # 1.Add an item
+    my_list.append(6)
+    print("After adding:",my_list)
+
+    # 2.Sort the list
+    my_list.sort()
+    print("After sorting:",my_list)
+
+    # 3.Reverse the list
+    my_list.reverse()
+    print("After reversing:",my_list)
+
+    # 4.Remove an item
+    my_list.remove(3)
+    print("After removing 3:",my_list)
+
+operations()
+
+
+
+
