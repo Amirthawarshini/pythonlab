ff7b7eb5beb64c88cd3cb1476fe4b8f925c030d9
Amirthawarshini <varshhhh08@gmail.com>
2025-03-12 15:17:16 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,14 @@
+# Set creation
+my_set = set([1, 2, 3, 4, 5])
+
+# Adding elements to the set
+my_set.add(6)
+
+# Removing elements from the set
+my_set.remove(2)
+
+# Set operations
+even_number = {2, 4, 6}
+print (my_set.union(even_number))  # Union of two sets
+print(my_set.intersection(even_number))  # Intersection of two sets
+print(my_set.difference(even_number))  # Difference of two sets
