62b86283ba958c4817bda083ee79b281875df1c0
Amirthawarshini <varshhhh08@gmail.com>
2025-04-02 15:00:03 +0530

    added lambda file

--- Function1.py
+++ Function1.py
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 def greet(name):
     print(f"Hello, {name}")
     
 greet("Alice")
 greet("Bob")
-=======
+
 def greet(name):
     print(f"Hello, {name}")
     
 greet("Alice")
 greet("Bob")
->>>>>>> 710fa2ce603dfcf118d3cbdb3d98c3973e0c732e
+
 greet("Charlie")
--- Strin.py
+++ Strin.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 string = input("Enter a string: ")
 for char in string:
-=======
-string = input("Enter a string: ")
+    string = input("Enter a string: ")
 for char in string:
->>>>>>> 710fa2ce603dfcf118d3cbdb3d98c3973e0c732e
     print(char)
