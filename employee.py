671df55c31fefa193fa30b67575ab3af04ac142e
Amirthawarshini <varshhhh08@gmail.com>
2025-04-02 15:12:54 +0530

    added a employee file

---
+++ employee.py
@@ -0,0 +1,8 @@
+name = input("Enter employee name: ")
+age = int(input("Enter employee age: "))
+salary = float(input("Enter employee salary: "))
+
+print(f"\nEmployee Details:")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Salary: {salary}")
